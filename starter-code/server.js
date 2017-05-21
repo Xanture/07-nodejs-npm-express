517639768f67b1e15bb75c9dcd26e4efde2eae63
Brian McFarlane <bmcfarlane82@gmail.com>
2017-05-21 12:38:04 -0700

    TODOs done!

--- starter-code/server.js
+++ starter-code/server.js
@@ -3,6 +3,9 @@
 // TODO: Initialize your project using NPM to create and populate a package.json file
 
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
+
+const express = require('express')
+const app = express();
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
@@ -10,9 +13,12 @@ const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'))
 
 // TODO: Write a new route that will handle a request and send the new.html file back to the user
-
+app.get('/new', function(require, response) {
+  response.sendFile('public/new.html', {root: '.'});
+})
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -24,4 +30,5 @@ app.post('/articles', bodyParser, function(request, response) {
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log('This server will cost this many scmekels' + PORT);
 });
